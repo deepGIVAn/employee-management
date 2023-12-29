4e210a01a3194f09f06a5ed5bef17255ddba4682
deepGIVAn <88539737+deepGIVAn@users.noreply.github.com>
2023-12-29 19:01:23 +0530

    layout

--- app/(home)/index.js
+++ app/(home)/index.js
@@ -1,8 +1,13 @@
 import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
-import { Feather } from "@expo/vector-icons";
-import { Entypo, Ionicons } from "@expo/vector-icons";
+import {
+  Entypo,
+  Ionicons,
+  Feather,
+  Octicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
 const index = () => {
   return (
@@ -23,7 +28,14 @@ const index = () => {
             <Entypo name="lock" size={24} color="black" />
           </View>
 
-          <View>
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 20,
+            }}
+          >
             <Pressable
               style={{
                 backgroundColor: "#D3CCE3",
@@ -31,6 +43,7 @@ const index = () => {
                 borderRadius: 6,
                 alignItems: "center",
                 justifyContent: "center",
+                flex: 1,
               }}
             >
               <View
@@ -49,6 +62,237 @@ const index = () => {
                 Employee List
               </Text>
             </Pressable>
+            <Pressable
+              style={{
+                backgroundColor: "#D3CCE3",
+                padding: 12,
+                borderRadius: 6,
+                alignItems: "center",
+                justifyContent: "center",
+                flex: 1,
+              }}
+            >
+              <View
+                style={{
+                  width: 50,
+                  height: 50,
+                  borderRadius: 25,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Ionicons name="ios-people" size={24} color="black" />
+              </View>
+              <Text style={{ marginTop: 7, fontWeight: 600 }}>
+                Mark Attendance
+              </Text>
+            </Pressable>
+          </View>
+          <View
+            style={{
+              marginTop: 20,
+              backgroundColor: "white",
+              paddingHorizontal: 10,
+              paddingVertical: 10,
+              borderRadius: 7,
+            }}
+          >
+            <Pressable
+              style={{
+                backgroundColor: "#BE93C5",
+                borderRadius: 6,
+                padding: 10,
+                flexDirection: "row",
+                alignItems: "center",
+                marginVertical: 10,
+              }}
+            >
+              <View
+                style={{
+                  padding: 7,
+                  width: 45,
+                  height: 45,
+                  borderRadius: 7,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Entypo name="newsletter" size={24} color="black" />
+              </View>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  fontWeight: 600,
+                  fontSize: 16,
+                  flex: 1,
+                }}
+              >
+                Attendance Report
+              </Text>
+              <View
+                style={{
+                  width: 35,
+                  height: 35,
+                  borderRadius: 7,
+                  backgroundColor: "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Entypo name="chevron-right" size={24} color="black" />
+              </View>
+            </Pressable>
+            <Pressable
+              style={{
+                backgroundColor: "#BE93C5",
+                borderRadius: 6,
+                padding: 10,
+                flexDirection: "row",
+                alignItems: "center",
+                marginVertical: 10,
+              }}
+            >
+              <View
+                style={{
+                  padding: 7,
+                  width: 45,
+                  height: 45,
+                  borderRadius: 7,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Octicons name="repo-pull" size={24} color="black" />
+              </View>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  fontWeight: 600,
+                  fontSize: 16,
+                  flex: 1,
+                }}
+              >
+                Summary Report
+              </Text>
+              <View
+                style={{
+                  width: 35,
+                  height: 35,
+                  borderRadius: 7,
+                  backgroundColor: "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Entypo name="chevron-right" size={24} color="black" />
+              </View>
+            </Pressable>
+            <Pressable
+              style={{
+                backgroundColor: "#BE93C5",
+                borderRadius: 6,
+                padding: 10,
+                flexDirection: "row",
+                alignItems: "center",
+                marginVertical: 10,
+              }}
+            >
+              <View
+                style={{
+                  padding: 7,
+                  width: 45,
+                  height: 45,
+                  borderRadius: 7,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Octicons name="report" size={24} color="black" />
+              </View>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  fontWeight: 600,
+                  fontSize: 16,
+                  flex: 1,
+                }}
+              >
+                All Generate Reports
+              </Text>
+              <View
+                style={{
+                  width: 35,
+                  height: 35,
+                  borderRadius: 7,
+                  backgroundColor: "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Entypo name="chevron-right" size={24} color="black" />
+              </View>
+            </Pressable>
+            <Pressable
+              style={{
+                backgroundColor: "#BE93C5",
+                borderRadius: 6,
+                padding: 10,
+                flexDirection: "row",
+                alignItems: "center",
+                marginVertical: 10,
+              }}
+            >
+              <View
+                style={{
+                  padding: 7,
+                  width: 45,
+                  height: 45,
+                  borderRadius: 7,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Ionicons name="ios-people-outline" size={24} color="black" />
+              </View>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  fontWeight: 600,
+                  fontSize: 16,
+                  flex: 1,
+                }}
+              >
+                Overtime Employees
+              </Text>
+              <View
+                style={{
+                  width: 35,
+                  height: 35,
+                  borderRadius: 7,
+                  backgroundColor: "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Entypo name="chevron-right" size={24} color="black" />
+              </View>
+            </Pressable>
+          </View>
+          <View>
+            <View>
+              <View>
+                <MaterialCommunityIcons
+                  name="guy-fawkes-mask"
+                  size={24}
+                  color="black"
+                />
+              </View>
+            </View>
           </View>
         </View>
       </LinearGradient>
